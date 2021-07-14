60ef45600cae19d2ca062a8d10b3d62650f6bc07
duriwa <66373641+duriwa@users.noreply.github.com>
2021-07-14 14:19:40 +0900

    8 ball random

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('magic 8 ball'),
+          ),
+          body: magic8ball(),
+
+        ),
+
       ),
     );
+
+class magic8ball extends StatefulWidget {
+  const magic8ball({Key key}) : super(key: key);
+
+  @override
+  _magic8ballState createState() => _magic8ballState();
+}
+
+class _magic8ballState extends State<magic8ball> {
+
+  int ballNumber = 2;
+
+  void changeBall(){
+    ballNumber = Random().nextInt(5)+1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+          children: [
+            Expanded(
+                child: FlatButton(
+                    onPressed: (){
+                      setState(() {
+                        changeBall();
+                      });
+                    }
+                    , child: Image.asset('images/ball$ballNumber.png'))
+            )
+          ],
+        ),
+    );
+  }
+}
